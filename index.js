031464c7aa04a3f8e4892ac81ef31f782658d0b5
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2019-10-29 20:52:21 +0530

    Server Running

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const path=require('path');
 const express =require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next)=>{
+    console.log('Hey');
+});
 
 app.listen(3000,()=>{
     console.log("server runnning");
